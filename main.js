6b703ab4476f8aef4441f13ad489d0a7ba67b1ed
Yaroslav <tvoibaget@mail.ru>
2023-04-24 23:29:58 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,9 @@
+new Swiper('.image-slider', {
+    loop: true,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev'
+    },
+    slidesPerView: 3,
+    // spaceBetween: -500,
+})
